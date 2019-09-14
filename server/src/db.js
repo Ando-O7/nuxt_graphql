1239738b889d99caa8786567a32b335c9af1e8c5
ando07 <ando.masahito@gmail.com>
2019-09-14 14:12:12 +0900

    [add]sample data

---
+++ server/src/db.js
@@ -0,0 +1,14 @@
+export let users = [
+    {
+        id: 1,
+        name: 'gatsby taro',
+        email: 'gatsby.taro@example.com',
+        age: 32
+      },
+      {
+        id: 2,
+        name: 'gridsome taro',
+        email: 'gridsom.taro@example.com',
+        age: 55
+      },
+];
