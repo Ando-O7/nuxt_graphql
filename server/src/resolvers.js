3b3c90a27c8764db329d55c98188485261d4a0d7
ando07 <ando.masahito@gmail.com>
2019-09-16 15:57:04 +0900

    [add]Mutation prpcess in resolvers

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -1,4 +1,4 @@
-import { users } from './db';
+import { users, generateId } from './db';
 
 const resolvers = {
   Query: {
@@ -6,6 +6,26 @@ const resolvers = {
     user: (parent, { id }, context, info) => users.find(user => user.id == id),
     // multiple search
     users: (parent, args, context, info) => users
+  },
+  Mutation: {
+    createUser: (parent, { name, email, age }, context, info) => {
+      const newUser = { id: generateId(), name, email, age};
+      users.push(newUser);
+      return newUser;
+    },
+    updateUser: (parent, { id, name, email, age }, context, info) => {
+      const updateUser = users.find(user => user.id == id);
+      updateUser.name = name;
+      updateUser.email = email;
+      updateUser.age = age;
+      return updateUser;
+    },
+    deleteUser: (parent, {id}, context, info) => {
+      const userIndex = users.findIndex(user => user.id == id);
+      if (userIndex === -1) throw new Error('User not found');
+      const [deleteUser] = users.splice(userIndex, 1);
+      return deleteUser;
+    }
   },
 };
 
