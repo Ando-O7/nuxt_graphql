0bfc72417ba8f0c548f38a96efa7ed2e52145eb7
ando07 <ando.masahito@gmail.com>
2019-09-17 13:17:39 +0900

    [add]graphpack.config set Subscription

---
+++ server/graphpack.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    server: {
+        subscriptions: {
+            // Set the same path as Query and Mutation 
+            // as the endpoint of Subscription.
+            path: `/graphql`
+        }
+    },
+};
