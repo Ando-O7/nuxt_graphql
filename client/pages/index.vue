b9409803e26f63d28f73417a8a91fef605b62155
ando07 <ando.masahito@gmail.com>
2019-09-21 22:27:03 +0900

    [add]button(sign up, edit, delete )

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,18 +1,32 @@
 <template>
-  <table>
-    <tr>
-      <th>ID</th>
-      <th>名前</th>
-      <th>メール</th>
-      <th>年齢</th>
-    </tr>
-    <tr v-for="item in users" :key="item.id">
-      <td>{{ item.id }}</td>
-      <td>{{ item.name }}</td>
-      <td>{{ item.email }}</td>
-      <td>{{ item.age }}</td>
-    </tr>
-  </table>
+  <div>
+    <button @click="isShowEditArea = true">
+      新規登録
+    </button>
+    
+    <table>
+        <tr>
+          <th>ID</th>
+          <th>名前</th>
+          <th>メール</th>
+          <th>年齢</th>
+        </tr>
+        <tr v-for="item in users" :key="item.id">
+          <td>{{ item.id }}</td>
+          <td>{{ item.name }}</td>
+          <td>{{ item.email }}</td>
+          <td>{{ item.age }}</td>
+          <td>
+            <button @click="editItem(item)">
+              編集
+            </button>
+            <button @click="deleteItem(item)">
+              削除
+            </button>
+          </td>
+        </tr>
+      </table>
+  </div>
 </template>
 
 <script>
